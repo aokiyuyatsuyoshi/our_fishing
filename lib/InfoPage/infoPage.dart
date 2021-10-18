fadd170a514174f9909c2f5861bf5cd55b527d45
aokiyuyatsuyoshi <rainarrow0114@ezweb.ne.jp>
2021-10-19 00:37:51 +0900

    feat:カルーセル、文字のアニメーションを追加

--- lib/InfoPage/infoPage.dart
+++ lib/InfoPage/infoPage.dart
@@ -1,4 +1,9 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled1/widget/posting_floating_button.dart';
+
+import 'carousel_widget.dart';
 
 class InfoPage extends StatelessWidget {
   @override
@@ -12,12 +17,86 @@ class InfoPage extends StatelessWidget {
           fit: BoxFit.fill,
         )),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text("info")],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      WavyAnimatedText('New Arrival News'),
+                      WavyAnimatedText('新着のニュース'),
+                    ],
+                    isRepeatingAnimation: true,
+                    onTap: () {
+                      print("Tap Event");
+                    },
+                  ),
+                ),
+                Text(
+                  "新着のニュース",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                Divider(
+                  thickness: 1.0,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: CarouselWithIndicatorDemo(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        WavyAnimatedText('New Arrival News'),
+                        WavyAnimatedText('新着のニュース'),
+                      ],
+                      isRepeatingAnimation: true,
+                      onTap: () {
+                        print("Tap Event");
+                      },
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        "みんなの投稿一覧",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  thickness: 1.0,
+                ),
+              ],
+            ),
           ),
         ),
       ),
+      floatingActionButton: postFloatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
